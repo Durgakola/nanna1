a4c6f530132bc621d0f1cbf0d047b5a3dc323201
Durgakola <Durgakola50@gmail.com>
2024-03-02 12:34:52 +0530

    mailz

--- forlloopdemo/ForDemo.java
+++ forlloopdemo/ForDemo.java
@@ -1,5 +1,37 @@
 package forlloopdemo;
 
 public class ForDemo {
+    public static void main(String[] args) {
+        //step:1 // for(int count=1;count<=100;count++){
+//            System.out.println("count "+count);
 
+        //infainet loop
+//        for(int count=1;count<=100;){
+//            System.out.println("count "+count);
+
+
+//        int array[] = {3, 4, 1, 5};
+//
+//        for (int i = 0; i < array.length; i++) {
+//
+//            System.out.println(array[i]);
+//        }
+//    }
+//}
+
+
+        int array[] = {3, 4, 1, 5};
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            System.out.println(array[i]);
+        }
+    }
 }
